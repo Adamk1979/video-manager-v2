eed64e5fc2f5027492be8b9ba18d47e40e3afaa1
Adamk1979 <adamkhalil040505@gmail.com>
2024-11-26 14:19:00 +0100

    removed file

--- src/database/index.js
+++
@@ -1,9 +0,0 @@
-// src/database/index.js
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('database_name', 'username', 'password', {
-  host: 'localhost', // Replace with your database host
-  dialect: 'mysql',
-});
-
-export default sequelize;
